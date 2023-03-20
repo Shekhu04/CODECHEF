c9fa8c268ba7981662eeb0d98644d6ad197549db
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2023-03-20 22:22:49 +0530

    Create WhoIsTaller.cpp

---
+++ WhoIsTaller.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+     int H1,H2;
+     cin>>H1>>H2;
+     if(H1>H2)cout<< "Abhishek" <<endl;
+     else cout<< "Harshal" <<endl;
+ 
+	return 0;
+}
